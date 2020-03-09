cb579068d3df89e3c089efe1fede4c16864e2488
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2020-03-09 19:50:04 +0300

    Initial commit

---
+++ api/post/read.php
@@ -0,0 +1,75 @@
+<?php
+
+  // Headers
+  // header('Access-Contol-Allow-Origin: *');
+  
+  // Allow from any origin
+  if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Content-Type: application/json');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+  }
+
+  // Access-Control headers are received during OPTIONS requests
+  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+  }
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Post.php';
+
+  // Instantiate DB & connect
+  $databse = new Database();
+  $db = $databse->connect();
+
+  // Instantiate blog post object
+  $post = new Post($db);
+
+  // Blog post query
+  $result = $post->read();
+  // Get row count
+  $num = $result->rowCount();
+
+  // Check if any posts
+  if($num > 0) {
+    // Post array
+    $posts_arr = array();
+    // $posts_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+
+      $post_item = array(
+        'id' => $id,
+        'title' => $title,
+        'body' => html_entity_decode($body),
+        'author' => $author,
+        'category_id' => $category_id,
+        'category_name' => $category_name,
+        'created_at' => $created_at
+      );
+
+      // Push to "data"
+      array_push($posts_arr, $post_item);
+      // array_push($posts_arr['data'], $post_item);
+    }
+
+    // Turn to JSON & output
+    echo json_encode($posts_arr);
+
+  } else {
+    // No Posts
+    echo json_encode(
+      array('message' => 'No Posts Found')
+    );
+  }
+
+?>
